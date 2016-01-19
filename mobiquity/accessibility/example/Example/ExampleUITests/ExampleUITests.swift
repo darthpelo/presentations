878306d2d7e2e2bff06d4b059f19ad4aa4d2bd48
Alessio Roberto <aroberto@mobiquityinc.com>
2016-01-20 00:04:52 +0100

    Add UI Test

--- mobiquity/accessibility/example/Example/ExampleUITests/ExampleUITests.swift
+++ mobiquity/accessibility/example/Example/ExampleUITests/ExampleUITests.swift
@@ -31,6 +31,16 @@ class ExampleUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let app = XCUIApplication()
+        app.tabBars.buttons["Second"].tap()
+        app.buttons["checkout"].tap()
+        app.buttons["pay"].tap()
+        
+        let label = app.staticTexts["second"].label
+        
+        XCTAssertEqual(label, "Second Section")
+        
     }
     
 }

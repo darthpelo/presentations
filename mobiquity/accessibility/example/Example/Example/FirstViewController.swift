d79a54823949c93928b387181d3e5a55316a260c
Alessio Roberto <aroberto@mobiquityinc.com>
2016-01-19 22:56:00 +0100

    First Commit

---
+++ mobiquity/accessibility/example/Example/Example/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Example
+//
+//  Created by Alessio Roberto on 19/01/16.
+//  Copyright © 2016 Alessio Roberto. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

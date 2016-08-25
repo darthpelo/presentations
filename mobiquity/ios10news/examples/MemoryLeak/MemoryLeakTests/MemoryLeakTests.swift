693b6569ed2c6c3fbd7323af7f4adff5c96dfcbc
Alessio Roberto <aroberto@mobiquityinc.com>
2016-08-25 10:49:54 +0200

    Update presentation and example

--- mobiquity/ios10news/examples/MemoryLeak/MemoryLeakTests/MemoryLeakTests.swift
+++ mobiquity/ios10news/examples/MemoryLeak/MemoryLeakTests/MemoryLeakTests.swift
@@ -33,7 +33,7 @@ class MemoryLeakTests: XCTestCase {
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        mainvc = nil
         nodevc = nil
         super.tearDown()
     }
